6890f268a1b5c342d284705ef06176e5fd188aef
Steve Britton <stvnbritton@gmail.com>
2017-05-10 16:29:18 -0400

    fix: Mocha tests.

--- test/acceptance-tests.js
+++ test/acceptance-tests.js
@@ -2,48 +2,28 @@
 
 'use strict';
 var assert = require('assert'),
-    exec = require('child_process').exec,
-    GULP = 'node_modules/gulp/bin/gulp.js',
-    veeva;
+    exec = require('child_process').exec;
 
-function gulp(task, done) {
-    exec(GULP + ' ' + task + ' --gulpfile examples/clm/test-gulpfile.js --silent', done);
-}
 
 function veeva(task, done) {
-    exec('index.js ' + ' ' + task, done);
+    exec('./bin/veeva.js ' + ' ' + task, done);
 }
 
 describe('acceptance tests for Veeva Module', function() {
 
-    it('module can be imported without blowing up', function() {
-        veeva = require('../index');
-
-        assert(veeva !== undefined);
-    });
 
     it('show help passes without error', function(done) {
-        gulp('--help', function(error, stdout, stderr) {
+        veeva('--help', function(error, stdout, stderr) {
             assert.ok(error === null);
             done();
-            console.log(stdout);
         });
     });
 
-     it('show configuration passes without error', function(done) {
-        gulp('--config', function(error, stdout, stderr) {
+    it('show config passes without error', function(done) {
+        veeva('--config', function(error, stdout, stderr) {
             assert.ok(error === null);
             done();
-            console.log(stdout);
         });
     });
 
-    /*
-    it('gulp assemble passes without error', function(done) {
-        gulp(' -k home assemble:test', function(error, stdout, stderr) {
-            assert.ok(error === null);
-            done();
-        });
-    });
-    */
 });
