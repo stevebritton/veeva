031e2434bcaedcd04da1a915affc6179f7a4cbe9
Steve Britton <stvnbritton@gmail.com>
2017-09-28 17:08:21 -0400

    Update node version detection

--- bin/veeva.js
+++ bin/veeva.js
@@ -4,39 +4,39 @@
 var chalk = require('chalk'),
     gulp = require('gulp'),
     pkg = require('../package.json'),
-    Q = require('q');
+    Q = require('q'),
+    utils = require('../lib/utils');
 
-var nodeVersion = process.version.replace('v',''),
-    nodeVersionRequired = pkg.engines.node.replace('>=','');
+var nodeVersion =  utils.getVersion(process.version.replace('v', '').split('.')),
+    requiredNodeVersion = utils.getVersion(pkg.engines.node.replace('>=','').split('.'));
+
 
 // check node version compatibility
-if(nodeVersion <= nodeVersionRequired){
-
+if(nodeVersion.major < requiredNodeVersion.major){
     console.log();
-    console.error(chalk.red.bold('✗ '), chalk.red.bold('Veeva requires node version ' + pkg.engines.node));
+    console.error(utils.log.chalk.red.bold('✗ '), utils.log.chalk.red.bold('NODE ' + process.version + ' was detected. Veeva requires node version ' + pkg.engines.node));
     console.log();
-
     process.exit(1);
 }
 
-var veeva = require('../index'),
-    args = [].slice.call(process.argv, 2),
-    exitCode = 0;
+var args = [].slice.call(process.argv, 2),
+    exitCode = 0,
+    veeva = require('../index');
 
 function checkForCommand(command) {
 
     var commands = [
-            'build',
-            'stage',
-            'deploy',
-            'veeva-vault-stage'
-        ];
+        'build',
+        'stage',
+        'deploy',
+        'veeva-vault-stage'
+    ];
 
     return (commands.indexOf(command) > -1);
 }
 
 
-veeva.cli(args).then(function(options) {
+return veeva.cli(args).then(function(options) {
 
     var deferred = Q.defer();
 
@@ -66,14 +66,12 @@ veeva.cli(args).then(function(options) {
 
     return deferred.promise;
 
-}).then(function(){
-    //process.exit(exitCode);
 }).catch(function(err) {
+
     exitCode = 1;
     console.log(('\n\n', chalk.red.bold('✗ '), err));
+
 });
 
 
-process.on('exit', function() {
-    //process.exit(exitCode);
-});
+
