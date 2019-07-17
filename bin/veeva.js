a7af3cd6ba082ada8b03cc826059d51c39d51231
Steve Britton <stvnbritton@gmail.com>
2019-07-17 12:06:17 -0400

    feat: Add new veeva screenshots command feat: Add screenshooting for all key messages, slides, and overlays describe in clm.yml files. feat: PDF generation of screenshots

--- bin/veeva.js
+++ bin/veeva.js
@@ -1,67 +1,65 @@
 #!/usr/bin/env node
 
 'use strict';
-var chalk = require('chalk'),
-    gulp = require('gulp'),
-    pkg = require('../package.json'),
-    utils = require('../lib/utils'),
-    veeva = require('../index');
+const chalk = require('chalk'),
+  gulp = require('gulp'),
+  pkg = require('../package.json'),
+  utils = require('../lib/utils'),
+  veeva = require('../index');
 
-var nodeVersion =  utils.getVersion(process.version.replace('v', '').split('.')),
-    requiredNodeVersion = utils.getVersion(pkg.engines.node.replace('>=', '').split('.'));
+const nodeVersion = utils.getVersion(process.version.replace('v', '').split('.')),
+  requiredNodeVersion = utils.getVersion(pkg.engines.node.replace('>=', '').split('.'));
 
 
 // check node version compatibility
 if (nodeVersion.major < requiredNodeVersion.major) {
-    console.log();
-    console.error(utils.log.chalk.red.bold('✗ '), utils.log.chalk.red.bold('NODE ' + process.version + ' was detected. Veeva requires node version ' + pkg.engines.node));
-    console.log();
-    process.exit(1);
+  console.log();
+  console.error(utils.log.chalk.red.bold('✗ '), utils.log.chalk.red.bold('NODE ' + process.version + ' was detected. Veeva requires node version ' + pkg.engines.node));
+  console.log();
+  process.exit(1);
 }
 
-var args = [].slice.call(process.argv, 2);
+const args = [].slice.call(process.argv, 2);
 
-function checkForCommand(command) {
+function checkForCommand (command) {
 
-    var commands = [
-        'build',
-        'stage',
-        'deploy',
-        'vault-stage'
-    ];
+  const commands = [
+    'build',
+    'deploy',
+    'screenshots',
+    'stage',
+    'vault-stage'
+  ];
 
-    return commands.indexOf(command) > -1;
+  return commands.includes(command);
 }
 
 
-return veeva.cli(args).then((options) => {
+return veeva.cli(args)
+    .then((options) => new Promise(function (resolve, reject) {
 
-    return new Promise(function (resolve, reject) {
+      // import gulp tasks
+      require('../lib/gulp')(gulp, options);
 
-        // import gulp tasks
-        require('../lib/gulp')(gulp, options);
+      const gulpCommand = checkForCommand(args[0]) ? args[0] : 'default';
 
-        var gulpCommand = checkForCommand(args[0]) ? args[0] : 'default';
+      console.log();
+      console.log(chalk.yellow.bold(' ⤷ Running veeva workflow: '), chalk.underline.yellow(gulpCommand));
+      console.log();
 
-        console.log();
-        console.log(chalk.yellow.bold(' ⤷ Running veeva workflow: '), chalk.underline.yellow(gulpCommand));
-        console.log();
-
-        /**
-         * @notes:
-         *     * gulp.start will be depreciated in Gulp v4
-         *     * replace with gulp.series
-         */
-        gulp.start(gulpCommand, function(err) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
+      /**
+       * @notes:
+       *     * gulp.start will be depreciated in Gulp v4
+       *     * replace with gulp.series
+       */
+      gulp.start(gulpCommand, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    }))
+    .catch((err) => {
+      console.error(('\n\n', chalk.red.bold('✗ '), err));
     });
-
-}).catch((err) => {
-    console.error(('\n\n', chalk.red.bold('✗ '), err));
-});
-
